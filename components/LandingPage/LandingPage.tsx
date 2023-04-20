b33b7ca41e1196ecedc8d5b0f257111a5ab6aaf7
sameer rasa <crazzywizard@gmail.com>
2023-04-20 13:34:39 -0400

    fixing ios issues

--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Header from "../Header"
 import SeoHead from "../SeoHead"
 import Community from "./sections/Community"
@@ -9,9 +10,9 @@ const LandingPage = () => (
   <div className="h-screen overflow-y-auto text-black bg-white">
     <SeoHead title="Cre8ors" description="Cre8ors" image="/CRE8ORSLOGO_ICON.svg" />
     <Header />
-    <Hero />
+    {/* <Hero />
     <Content />
-    <Community />
+    <Community /> */}
   </div>
 )
 
