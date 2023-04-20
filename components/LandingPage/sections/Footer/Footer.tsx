f4c7942c8dddc35a5858afe9a77b64867f0d991e
sameer rasa <crazzywizard@gmail.com>
2023-04-20 13:01:11 -0400

    fixing ios issues

--- components/LandingPage/sections/Footer/Footer.tsx
+++ components/LandingPage/sections/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+// import { useState } from "react"
 import Image from "next/image"
 import { motion } from "framer-motion"
 import Link from "next/link"
@@ -28,36 +28,22 @@ const CTA_BUTTONS = [
     link: "https://cre8ors.beehiiv.com/?_gl=1*16y8c82*_ga*NzQ0MjY2NzkyLjE2NzI4NzY1OTY.*_ga_E6Y4WLQ2EC*MTY4MTQ5NTA5Ny4zNS4xLjE2ODE0OTUxMDAuNTcuMC4w",
   },
 ]
-const Footer = () => {
-  const [isHovered, setIsHovered] = useState({
-    twitter: false,
-    discord: false,
-    slack: false,
-    newsletter: false,
-  })
-  return (
-    <footer className="absolute flex flex-row items-center w-full text-white lg:md:justify-center lg:md:mt-24 lg:md:py-4">
-      {CTA_BUTTONS.map((button) => (
-        <motion.div
-          key={button.alt}
-          className="cursor-pointer"
-          whileHover={{ scale: 0.9 }}
-          onMouseEnter={() => setIsHovered({ ...isHovered, [button.alt]: true })}
-          onMouseLeave={() => setIsHovered({ ...isHovered, [button.alt]: false })}
-        >
-          <Link href={button.link || "/"}>
-            <Image
-              src={isHovered[button.alt] ? button.srcHover : button.src}
-              alt={button.alt}
-              width={400}
-              height={200}
-              loader={customLoader}
-            />
-          </Link>
-        </motion.div>
-      ))}
-    </footer>
-  )
-}
+const Footer = () => (
+  <footer className="absolute flex flex-row items-center w-full text-white lg:md:justify-center lg:md:mt-24 lg:md:py-4">
+    {CTA_BUTTONS.map((button) => (
+      <motion.div key={button.alt} className="cursor-pointer" whileHover={{ scale: 0.9 }}>
+        <Link href={button.link || "/"}>
+          <Image
+            src={button.srcHover}
+            alt={button.alt}
+            width={400}
+            height={200}
+            loader={customLoader}
+          />
+        </Link>
+      </motion.div>
+    ))}
+  </footer>
+)
 
 export default Footer
