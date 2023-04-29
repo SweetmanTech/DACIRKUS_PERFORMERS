1873f6734ffb233b99332b727d06e9df1926fc78
sameer rasa <crazzywizard@gmail.com>
2023-04-29 17:51:54 -0400

    adding link

--- components/TeamsPage/components/Cards/Founding/FoundingMembersCard.tsx
+++ components/TeamsPage/components/Cards/Founding/FoundingMembersCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 import { FC, useState } from "react"
 
 interface FoundingMembersCardProps {
@@ -16,19 +17,21 @@ const FoundingMembersCard: FC<FoundingMembersCardProps> = ({ profilePic, twitter
           </div>
         </div>
         <h2 className="text-lg text-gray-800 ">{twitterHandle || "twitter_handle"}</h2>
-        <button
-          type="button"
-          className="px-4 py-2 mt-4"
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-        >
-          <Image
-            src={hovered ? "/hover/twitter_hover_button.png" : "/twitter_button.png"}
-            alt="Profile picture"
-            width={45}
-            height={28}
-          />
-        </button>
+        <Link href={`https://twitter.com/${twitterHandle}`}>
+          <button
+            type="button"
+            className="px-4 py-2 mt-4"
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+          >
+            <Image
+              src={hovered ? "/hover/twitter_hover_button.png" : "/twitter_button.png"}
+              alt="Profile picture"
+              width={45}
+              height={28}
+            />
+          </button>
+        </Link>
       </div>
     </div>
   )
