c5677d3562a2a7634251257c27c0030c3b64d7b9
sameer rasa <crazzywizard@gmail.com>
2023-05-15 18:40:50 -0400

    fixing scroll and button bugs

--- components/LandingPage/sections/Community/Community.tsx
+++ components/LandingPage/sections/Community/Community.tsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import Image from "next/image"
 import { motion } from "framer-motion"
 import Link from "next/link"
-import customLoader from "../../../../lib/customLoader"
 import Footer from "../Footer"
 
 const Community = () => {
@@ -76,15 +75,9 @@ const Community = () => {
   ]
   return (
     <div className="relative flex flex-col items-center justify-center mt-24">
-      <Image
-        src="/CITYSCAPE2.png"
-        alt="cityscape"
-        width={1988}
-        height={3071}
-        loader={customLoader}
-      />
+      <Image src="/CITYSCAPE2.svg" alt="cityscape" width={1988} height={3071} />
       <div className="absolute z-10 justify-center cursor-pointer lg:md:mt-10 lg:top-96 md:top-80 top-24 lg:md:top-36 samsungS8:top-10 ">
-        <div className="flex gap-5 p-4 pr-4 overflow-x-auto border-b-4 border-black scrollbar-thumb-offset-8 snap-x snap-mandatory flex-nowrap scroll-h-96 scroll-pl-6 scrollbar scrollbar-thumb-red-600 scrollbar-thumb-rounded-lg scrollbar-track-gray-100 scrollbar-track-rounded-lg scroll-m-20">
+        <div className="flex gap-5 p-4 pr-4 overflow-x-auto border-b-4 border-black scrollbar-thumb-offset-8 flex-nowrap scroll-h-96 scroll-pl-6 scrollbar scrollbar-thumb-red-600 scrollbar-thumb-rounded-lg scrollbar-track-gray-100 scrollbar-track-rounded-lg scroll-m-20">
           {COMMUNITY_CARDS.map((card) => (
             <motion.div
               key={card.title}
