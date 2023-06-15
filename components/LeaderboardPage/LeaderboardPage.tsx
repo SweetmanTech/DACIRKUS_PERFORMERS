315a86bd0755e0b8aefefa67a5f4ae7541f1bc11
Software Engineer <im.ziad.sakr@gmail.com>
2023-06-15 19:37:02 -0400

    update leaderboard page & team page

--- components/LeaderboardPage/LeaderboardPage.tsx
+++ components/LeaderboardPage/LeaderboardPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import * as _ from "lodash"
+import { useMediaQuery } from "usehooks-ts"
 import getOwnersForCollection from "../../lib/alchemy/getOwnersForCollection"
 import getParticipants from "../../lib/getParticipants"
 import LeaderboardRow from "./LeaderboardRow"
@@ -8,6 +9,7 @@ import Layout from "../Layout"
 
 const LeaderboardPage = () => {
   const [collectors, setCollectors] = useState([])
+  const isMobile = useMediaQuery("(max-width: 768px)")
 
   useEffect(() => {
     const fetchTopCollectors = async () => {
@@ -30,23 +32,31 @@ const LeaderboardPage = () => {
   return (
     <Layout type="contained">
       <div className="w-full pt-24 mx-auto">
-        <div className="font-[eigerdals] dark:text-white text-center text-[2.5rem] md:text-[4rem] font-bold pt-6">
+        <div
+          className="
+          font-[eigerdals] 
+          dark:text-white text-center 
+          text-[40px] md:text-[75px] 
+          font-bold pt-6
+        "
+        >
           Leaderboard
         </div>
         <div className="w-full flex justify-center pb-4">
           <div
             className="font-quicksand 
             dark:text-white text-center 
-            max-w-[400px] md:max-w-[550px] 
-            text-[1rem] md:text-[1.5rem] 
-            font-[550]"
+            w-[300px] xs:w-[350px] md:w-[500px] 
+            text-[13px] xs:text-[15px] md:text-[18px] 
+            drop-shadow-[0_2px_2px_rgba(0,0,0,0.45)] 
+            font-[500]"
           >
             Currently Tracking: Divine Ancestral Pendants Collect and burn 88 to redeem a Passport
           </div>
         </div>
         <div className="md:px-4 w-full flex justify-center">
           <div
-            className="w-[370px] md:w-full 
+            className="w-[310px] xs:w-[370px] md:w-full 
             border-[2px] border-[black] border-solid
             h-[470px] rounded-lg 
             overflow-auto 
@@ -58,16 +68,43 @@ const LeaderboardPage = () => {
             <table className="w-full font-quicksand bg-white">
               <thead className="border-b-[2px] border-black border-solid">
                 <tr>
-                  <th className="p-4 text-left border-r-[2px] border-black text-center uppercase min-w-[100px]">
+                  <th
+                    className="p-[5px] md:p-4 
+                    text-left border-r-[2px] 
+                    border-black text-center
+                    uppercase 
+                    text-[8px] xs:text-[11px] md:text-[18px]
+                    md:min-w-[100px]"
+                  >
                     Rank
                   </th>
-                  <th className="p-4 text-left border-r-[2px] border-black text-center uppercase min-w-[200px]">
-                    # of NFTs Owned
+                  <th
+                    className="p-[5px] md:p-4 
+                    text-left border-r-[2px] 
+                    border-black text-center 
+                    uppercase 
+                    text-[8px] xs:text-[11px] md:text-[18px]
+                    w-[100px] xs:!w-[130px] md:!w-[200px]"
+                  >
+                    # of NFTs {!isMobile ? "Owned" : ""}
                   </th>
-                  <th className="p-4 text-left border-r-[2px] border-black text-center uppercase">
+                  <th
+                    className="p-[5px] md:p-4 
+                    text-left border-r-[2px] 
+                    border-black text-center 
+                    uppercase 
+                    text-[8px] xs:text-[11px] md:text-[18px]"
+                  >
                     Address
                   </th>
-                  <th className="p-4 text-left text-center uppercase">Twitter</th>
+                  <th
+                    className="p-[5px] md:p-4 
+                    text-left text-center 
+                    uppercase 
+                    text-[8px] xs:text-[11px] md:text-[18px]"
+                  >
+                    Twitter
+                  </th>
                 </tr>
               </thead>
               {collectors.length > 0 ? (
