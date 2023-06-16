e847f2c787ff07880424103834506588c59b410b
Software Engineer <im.ziad.sakr@gmail.com>
2023-06-15 20:25:27 -0400

    fix code review

--- components/LeaderboardPage/LeaderboardRow.tsx
+++ components/LeaderboardPage/LeaderboardRow.tsx
@@ -24,8 +24,7 @@ const LeaderboardRow = ({ address, numberOwned, rank, twitterHandle }) => {
   return (
     <tr key={address} className="text-center bg-white hover:bg-blue-300">
       <td
-        className="
-        text-[8px] xs:text-[11px] md:text-[16px]
+        className="text-[8px] xs:text-[11px] md:text-[16px]
         p-[5px] md:px-4 md:py-2 border-r-2 border-black"
       >
         #{rank}
