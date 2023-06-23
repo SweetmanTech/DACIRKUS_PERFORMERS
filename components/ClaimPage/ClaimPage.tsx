d6868ae936d818ae0b7ad1b8d07f5608d3505430
Software Engineer <im.ziad.sakr@gmail.com>
2023-06-23 06:40:08 -0400

    claim page feedback
    all content should be on Paper background.
    Placement & Footer Gap
    Typo, Button Width, Spacing & X
    Gap between text

--- components/ClaimPage/ClaimPage.tsx
+++ components/ClaimPage/ClaimPage.tsx
@@ -93,11 +93,13 @@ const ClaimPage = () => {
                   <div ref={titleRef}>
                     <SectionTitle
                       text="Exchange Ticket, Redeem Passport"
-                      className="mx-[8px] xs:m-6 w-[290px] samsungS8:w-[330px] !text-[30px] samsungS8:!text-[33px] lg:!text-[64px] lg:w-[550px] md:text-left mb-4"
+                      className="mx-[8px] xs:m-6 w-[290px] samsungS8:w-[330px] 
+                        !text-[30px] samsungS8:!text-[33px] lg:!text-[64px] lg:w-[550px] md:text-left mb-4
+                        md:leading-[102.3%]"
                     />
                   </div>
                   <div className="flex justify-center" ref={contentRef}>
-                    <SectionContent className="w-[290px] samsungS8:w-[300px] md:w-[550px] m-[8px] mt-[30px] xs:mt-[20px] md:mt-12 md:text-left">
+                    <SectionContent className="w-[290px] samsungS8:w-[300px] md:w-[550px] m-[8px] mt-[30px] xs:mt-[20px] md:mt-[45px] md:text-left">
                       <div className="pl-0 xs:pl-4">
                         1. Connect wallet
                         <br />
@@ -108,7 +110,7 @@ const ClaimPage = () => {
                       </div>
                     </SectionContent>
                   </div>
-                  <div className="px-12 flex justify-center md:justify-start">
+                  <div className="px-12 flex justify-center md:justify-start md:mt-[15px]">
                     <Popover className="w-full" id="connect_popver_claim">
                       <div ref={buttonRef}>
                         <Button
@@ -130,7 +132,7 @@ const ClaimPage = () => {
                     </Popover>
                   </div>
                 </div>
-                <div className="justify-center md:flex hidden">
+                <div className="justify-center md:flex hidden md:translate-y-[30px]">
                   {width && (
                     <Media
                       type="image"
--- components/ClaimPage/ConnetWallet.tsx
+++ components/ClaimPage/ConnetWallet.tsx
@@ -8,32 +8,76 @@ interface ConnectWalletProps {
 const ConnectWallet = ({ handleClose }: ConnectWalletProps) => (
   <div
     className="bg-[url('/assets/Common/popup.svg')] w-full m-6 md:w-[400px] h-[400px] samsungS8:h-[500px] md:h-[600px] bg-center shadow-[0px_5px_9px_rgba(0,0,0,0.25)]
-            rounded-[20px] flex flex-col justify-center items-center gap-[20px] relative"
+            rounded-[20px] flex flex-col justify-center items-center gap-[35px] relative"
   >
-    <div className="bg-[black] absolute top-[10px] right-[10px] w-6 h-6 m-2 rounded-full cursor-pointer">
+    <div
+      className="bg-[black] absolute top-[10px] right-[10px] w-6 h-6 m-2 rounded-full cursor-pointer
+      flex justify-center items-center"
+    >
       <svg
-        xmlns="http://www.w3.org/2000/svg"
+        width="13"
+        height="12"
+        viewBox="0 0 13 12"
         fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke="white"
+        xmlns="http://www.w3.org/2000/svg"
         onClick={handleClose}
       >
-        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+        <line
+          x1="2.84836"
+          y1="1.77502"
+          x2="10.273"
+          y2="9.19965"
+          stroke="url(#paint0_linear_1383_155)"
+          strokeWidth="3"
+          strokeLinecap="round"
+        />
+        <line
+          x1="2.72705"
+          y1="9.1996"
+          x2="10.1517"
+          y2="1.77498"
+          stroke="url(#paint1_linear_1383_155)"
+          strokeWidth="3"
+          strokeLinecap="round"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_1383_155"
+            x1="5.50001"
+            y1="6.548"
+            x2="4.7929"
+            y2="7.2551"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0.40625" stop-color="white" />
+            <stop offset="0.578125" stop-color="#8D8DDA" />
+          </linearGradient>
+          <linearGradient
+            id="paint1_linear_1383_155"
+            x1="7.50002"
+            y1="6.54795"
+            x2="8.20713"
+            y2="7.25506"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0.40625" stop-color="white" />
+            <stop offset="0.578125" stop-color="#8D8DDA" />
+          </linearGradient>
+        </defs>
       </svg>
     </div>
-    <div className="font-eigerdals text-[36px]">Connect</div>
+    <div className="font-eigerdals text-[36px] mb-[10px]">Connect</div>
     <WalletConnectButton>
       <Button
         id="wallet_connect_btn_in_redeem"
-        className="!uppercase w-[200px] !bg-[black] !text-white !shadow-[0px_4px_4px_rgb(0,0,0,0.25)]"
+        className="!uppercase w-[270px] !bg-[black] !text-white !shadow-[0px_4px_4px_rgb(0,0,0,0.25)]"
       >
         Wallet Connect
       </Button>
     </WalletConnectButton>
     <Button
       id="cross_mint_btn_in_redeem"
-      className="!uppercase w-[200px] !bg-[black] !text-white !shadow-[0px_4px_4px_rgb(0,0,0,0.25)]"
+      className="!uppercase w-[270px] !bg-[black] !text-white !shadow-[0px_4px_4px_rgb(0,0,0,0.25)]"
     >
       Crossmint
     </Button>
