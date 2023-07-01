1c8b51913a6715648c65c62db25d295882a035d0
sameer rasa <crazzywizard@gmail.com>
2023-07-01 12:08:14 -0400

    updating admin auth protection

--- middleware/auth.middleware.ts
+++ middleware/auth.middleware.ts
@@ -5,9 +5,11 @@ import {
   NotFoundException,
   UnauthorizedException,
 } from "next-api-decorators"
+import { Magic } from "@magic-sdk/admin"
 import Auth from "../Models/Auth"
 import dbConnect from "../utils/db"
 
+const mAdmin = new Magic(process.env.MAGIC_SECRET_KEY)
 export const getApiKeys = async (collectionName) => {
   await dbConnect()
   const result = Auth.findOne({ collectionName }).lean()
@@ -32,6 +34,21 @@ const handler = async (
   next()
 }
 
+export const AdminAuthGuard = createMiddlewareDecorator(
+  async (req: NextApiRequest, res: NextApiResponse, next: NextFunction) => {
+    const { authorization } = req.headers
+    if (!authorization) {
+      throw new NotFoundException("Authorization header not found")
+    }
+    const [, token] = authorization.split(" ")
+    try {
+      mAdmin.token.validate(token)
+    } catch (e) {
+      throw new UnauthorizedException("Invalid token")
+    }
+    next()
+  },
+)
 export const AllowListAuthGuard = createMiddlewareDecorator(
   async (req: NextApiRequest, res: NextApiResponse, next: NextFunction) => {
     handler(req, res, next, "AllowList")
