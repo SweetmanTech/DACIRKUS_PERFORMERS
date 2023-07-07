453574447f510d4655f10ab574164fae35317cd6
sameer rasa <crazzywizard@gmail.com>
2023-07-06 23:39:37 -0400

    fixing button

--- components/LoginPage/LoginPage.tsx
+++ components/LoginPage/LoginPage.tsx
@@ -30,7 +30,7 @@ const LoginPage = () => {
             </div>
             <button
               type="submit"
-              className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               onClick={handleLogin}
             >
               Send Magic Link
