81f3be25a7611fbb5f2056a90a6b63b6b47d9ceb
Software Engineer <im.ziad.sakr@gmail.com>
2023-07-12 09:57:06 -0400

    add title

--- components/MintPage/MintPage.tsx
+++ components/MintPage/MintPage.tsx
@@ -43,7 +43,9 @@ const MintPage = () => {
               className="max-w-[1280px] flex-grow flex flex-col justify-end md:flex-row items-center 
             pb-[250px] samsungS8:pb-[270px] xs:pb-[290px] md:pb-0 relative z-[100]"
             >
-             
+             <div className="dark:bg-[#00000069] rounded-[20px] py-[40px]">
+                
+             </div>
             </div>
             <Footer className="!pt-0 !pb-0 !bg-transparent relative z-[10]" />
           </div>
