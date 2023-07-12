1ab5a8f94bac34b7b3067bdd4c343ec55587a63d
Software Engineer <im.ziad.sakr@gmail.com>
2023-07-12 13:59:51 -0400

    fixed resolution issue

--- components/MintPage/MintPage.tsx
+++ components/MintPage/MintPage.tsx
@@ -12,8 +12,8 @@ import useShakeEffect from "../../hooks/useShakeEffect"
 import Media from "../../shared/Media"
 
 const MintPage = () => {
-  const [containerRef, { width }] = useMeasure()
-  const isResponsive = useMediaQuery("(max-width: 1440px)")
+  const [containerRef, { height }] = useMeasure()
+  const isResponsive = useMediaQuery("(max-width: 1020px)")
   const isMobile = useMediaQuery("(max-width: 768px)")
   const { themeMode } = useTheme()
 
@@ -43,13 +43,11 @@ const MintPage = () => {
         className="relative overflow-y-auto min-h-[100vh] overflow-x-hidden z-[1] w-[100vw]"
         ref={containerRef}
       >
-        {width && (
+        {height && (
           <div
             className="relative z-[6] flex flex-col items-center pt-[80px]"
             style={{
-              width: `${width}px`,
-              height: isResponsive ? `${(980 / 430) * width}px` : `${(1020 / 1440) * width}px`,
-              minHeight: isResponsive ? "100vh" : "",
+              height: "100vh",
               backgroundImage:
                 // eslint-disable-next-line no-nested-ternary
                 themeMode === "light"
@@ -62,36 +60,58 @@ const MintPage = () => {
               backgroundPosition: isResponsive
                 ? isMobile
                   ? `bottom center`
-                  : `bottom right`
-                : `center`,
+                  : `bottom left`
+                : `bottom left`,
             }}
           >
+            <div className="absolute left-0 bottom-0 z-[3] md:block hidden">
+              <Media
+                link="/assets/Mint/character.png"
+                type="image"
+                containerStyle={{
+                  height: `${(height / 1020) * 760}px`,
+                  width: `${(height / 1020) * 380}px`,
+                }}
+              />
+            </div>
             <div
               className="max-w-[1280px] flex-grow flex flex-col justify-end md:flex-row items-center 
             pb-[180px] samsungS8:pb-[220px] xs:pb-[260px] md:pb-0 relative z-[100]"
             >
               <div
                 className="dark:bg-[#00000069] rounded-[20px] 
-              md:pt-[40px] md:pb-[20px] md:px-[40px]
+              xl:pt-[40px] xl:pb-[20px] xl:px-[40px]
               px-[20px] py-[30px]
               dark:border-[1px] dark:border-[black]
               flex flex-col items-center 
               gap-[20px] md:gap-[30px]
              "
               >
-                <Title text="Mint Your Cre8ors" className="!text-[28px] xs:!text-[35px]" />
+                <Title
+                  text="Mint Your Cre8ors"
+                  className="!text-[28px] xs:!text-[35px] md:!text-[50px] xl:!text-[65px]"
+                />
                 <div ref={passportRef}>
-                  <Button id="passports_btn_mint" className="w-[250px] xs:w-[280px] md:w-[592px]">
+                  <Button
+                    id="passports_btn_mint"
+                    className="w-[250px] xs:w-[280px] md:w-[470px] xl:w-[592px]"
+                  >
                     Passports
                   </Button>
                 </div>
                 <div ref={allowlistRef}>
-                  <Button id="allowlist_btn_mint" className="w-[250px] xs:w-[280px] md:w-[592px]">
+                  <Button
+                    id="allowlist_btn_mint"
+                    className="w-[250px] xs:w-[280px] md:w-[470px] xl:w-[592px]"
+                  >
                     Allowlist
                   </Button>
                 </div>
                 <div ref={publicRef}>
-                  <Button id="public_btn_mint" className="w-[250px] xs:w-[280px] md:w-[592px]">
+                  <Button
+                    id="public_btn_mint"
+                    className="w-[250px] xs:w-[280px] md:w-[470px] xl:w-[592px]"
+                  >
                     Public
                   </Button>
                 </div>
