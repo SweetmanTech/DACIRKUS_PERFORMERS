b12c7e10376356b1924345e0f92eb30d03254b52
Software Engineer <im.ziad.sakr@gmail.com>
2023-07-14 12:17:03 -0400

    add footer

--- components/MintPage/MintPage.tsx
+++ components/MintPage/MintPage.tsx
@@ -6,6 +6,7 @@ import PFPs from "./PFPs"
 import Archetypes from "./Archetypes"
 import InHouse from "./InHouse"
 import Collaborate from "./Collaborate"
+import Footer from "../Footer"
 
 const MintPage = () => {
   const isXl = useMediaQuery("(max-width: 1150px)")
@@ -20,6 +21,7 @@ const MintPage = () => {
         <InHouse />
         {isXl && <Archetypes />}
         {!isXl && <Collaborate />}
+        <Footer className="pt-0"/>
       </div>
     </Layout>
   )
