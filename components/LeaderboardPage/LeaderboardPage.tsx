271659ba4ac850e6d50d5e3f383db35c4edce9b8
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-19 21:08:49 -0300

    I remove unused components

--- components/LeaderboardPage/LeaderboardPage.tsx
+++ components/LeaderboardPage/LeaderboardPage.tsx
@@ -5,7 +5,6 @@ import getOwnersForCollection from "../../lib/alchemy/getOwnersForCollection"
 import getParticipants from "../../lib/getParticipants"
 import LeaderboardRow from "./LeaderboardRow"
 import SkeletonTableBody from "./SkeletonTableBody"
-import Layout from "../Layout"
 
 const LeaderboardPage = () => {
   const [collectors, setCollectors] = useState([])
@@ -30,33 +29,32 @@ const LeaderboardPage = () => {
   }, [])
 
   return (
-    <Layout type="contained">
-      <div className="w-full pt-24 mx-auto">
-        <div
-          className="
+    <div className="w-full pt-24 mx-auto">
+      <div
+        className="
           font-[eigerdals] 
           dark:text-white text-center 
           text-[40px] md:text-[75px] 
           font-bold pt-6
         "
-        >
-          Leaderboard
-        </div>
-        <div className="w-full flex justify-center pb-4">
-          <div
-            className="font-quicksand 
+      >
+        Leaderboard
+      </div>
+      <div className="w-full flex justify-center pb-4">
+        <div
+          className="font-quicksand 
             dark:text-white text-center 
             w-[300px] xs:w-[350px] md:w-[430px] 
             text-[13px] xs:text-[15px] md:text-[18px] 
             drop-shadow-[0_2px_2px_rgba(0,0,0,0.45)] 
             font-[500]"
-          >
-            Currently Tracking: Divine Ancestral Pendants Collect and burn 88 to redeem a Passport
-          </div>
+        >
+          Currently Tracking: Divine Ancestral Pendants Collect and burn 88 to redeem a Passport
         </div>
-        <div className="md:px-4 w-full flex justify-center">
-          <div
-            className="w-[310px] xs:w-[370px] md:w-full 
+      </div>
+      <div className="md:px-4 w-full flex justify-center">
+        <div
+          className="w-[310px] xs:w-[370px] md:w-full 
             border-[2px] border-[black] border-solid
             h-[470px] rounded-lg 
             overflow-auto 
@@ -64,69 +62,68 @@ const LeaderboardPage = () => {
             scrollbar scrollbar-thumb-[black] 
             scrollbar-track-white 
             scrollbar-thumb-rounded-full"
-          >
-            <table className="w-full font-quicksand bg-white">
-              <thead className="border-b-[2px] border-black border-solid">
-                <tr>
-                  <th
-                    className="p-[5px] md:p-4 
+        >
+          <table className="w-full font-quicksand bg-white">
+            <thead className="border-b-[2px] border-black border-solid">
+              <tr>
+                <th
+                  className="p-[5px] md:p-4 
                     text-left border-r-[2px] 
                     border-black text-center
                     uppercase 
                     text-[8px] xs:text-[11px] md:text-[18px]
                     md:min-w-[100px]"
-                  >
-                    Rank
-                  </th>
-                  <th
-                    className="p-[5px] md:p-4 
+                >
+                  Rank
+                </th>
+                <th
+                  className="p-[5px] md:p-4 
                     text-left border-r-[2px] 
                     border-black text-center 
                     uppercase 
                     text-[8px] xs:text-[11px] md:text-[18px]
                     w-[100px] xs:!w-[130px] md:!w-[200px]"
-                  >
-                    # of NFTs {!isMobile ? "Owned" : ""}
-                  </th>
-                  <th
-                    className="p-[5px] md:p-4 
+                >
+                  # of NFTs {!isMobile ? "Owned" : ""}
+                </th>
+                <th
+                  className="p-[5px] md:p-4 
                     text-left border-r-[2px] 
                     border-black text-center 
                     uppercase 
                     text-[8px] xs:text-[11px] md:text-[18px]"
-                  >
-                    Address
-                  </th>
-                  <th
-                    className="p-[5px] md:p-4 
+                >
+                  Address
+                </th>
+                <th
+                  className="p-[5px] md:p-4 
                     text-left text-center 
                     uppercase 
                     text-[8px] xs:text-[11px] md:text-[18px]"
-                  >
-                    Twitter
-                  </th>
-                </tr>
-              </thead>
-              {collectors.length > 0 ? (
-                <tbody>
-                  {collectors.map((collector, index) => (
-                    <LeaderboardRow
-                      key={collector.walletAddress}
-                      address={collector.walletAddress}
-                      numberOwned={collector.nftsOwned}
-                      twitterHandle={collector.twitterHandle}
-                      rank={index + 1}
-                    />
-                  ))}
-                </tbody>
-              ) : (
-                <SkeletonTableBody />
-              )}
-            </table>
-          </div>
+                >
+                  Twitter
+                </th>
+              </tr>
+            </thead>
+            {collectors.length > 0 ? (
+              <tbody>
+                {collectors.map((collector, index) => (
+                  <LeaderboardRow
+                    key={collector.walletAddress}
+                    address={collector.walletAddress}
+                    numberOwned={collector.nftsOwned}
+                    twitterHandle={collector.twitterHandle}
+                    rank={index + 1}
+                  />
+                ))}
+              </tbody>
+            ) : (
+              <SkeletonTableBody />
+            )}
+          </table>
         </div>
       </div>
-    </Layout>
+    </div>
   )
 }
 
