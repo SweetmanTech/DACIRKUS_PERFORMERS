826ee5b38682ce5c400d93291ff467c2b3d2f4a8
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-22 13:50:55 -0300

    I update game to fill full screen.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,12 @@ const Game = () => {
     const winAny = window as any
     winAny.receiveGameData = (data) => {
       console.log("SWEETS Received from Godot:", data)
-      // Handle your data here, maybe using React's state or context
     }
   }, [])
 
   return (
     <div>
-      <iframe src="/game/index.html" title="Hypersurveilled" className="h-[90vh] w-[100vw]" />
+      <iframe src="/game/index.html" title="Hypersurveilled" className="h-[100vh] w-[100vw]" />
     </div>
   )
 }
