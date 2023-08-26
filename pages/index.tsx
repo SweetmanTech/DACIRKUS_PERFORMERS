c84383497d58cc8791769a631cb9872c2c4e8fe7
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-26 15:01:56 -0300

    I rollback index changes.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,7 @@
-import { useEffect } from "react"
-
-const Game = () => {
-  useEffect(() => {
-    const winAny = window as any
-    winAny.receiveGameData = (data) => {
-      // eslint-disable-next-line no-console
-      console.log("SWEETS Received from Godot:", data)
-    }
-  }, [])
-
-  return (
-    <div>
-      <iframe src="/game/index.html" title="Hypersurveilled" className="h-[80vh] w-[100vw]" />
-    </div>
-  )
-}
+const Game = () => (
+  <div>
+    <iframe src="/game/index.html" title="Hypersurveilled" className="h-[100vh] w-[100vw]" />
+  </div>
+)
 
 export default Game
