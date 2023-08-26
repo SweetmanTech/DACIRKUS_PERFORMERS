cce2b1e5ec3afad28253cf8a5a2b0c67a7b8dbfa
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-26 15:47:50 -0300

    I remove dev comments.

--- lib/firebase/firebase.tsx
+++ lib/firebase/firebase.tsx
@@ -1,8 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore, query, orderBy, limit, collection, getDocs } from "firebase/firestore"
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "relief-game.firebaseapp.com",
@@ -12,14 +10,12 @@ const firebaseConfig = {
   appId: "1:533966397288:web:911f9f0d539025183100dd",
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
-// Initialize Cloud Firestore and get a reference to the service
-export const firebaseFirestore = getFirestore(app)
+const firestore = getFirestore(app)
 
 export const getLeaderboard = async () => {
-  const citiesRef = collection(firebaseFirestore, "userdata")
+  const citiesRef = collection(firestore, "userdata")
   const q = query(citiesRef, orderBy("score", "desc"), limit(3))
   const querySnapshot = await getDocs(q)
   const scores = []
