4fd615b70ada6c956ef6d84fc52d05a6e0da5035
Sweets Sweetman <sweetmantech@gmail.com>
2023-10-04 16:42:25 -0300

    I pass hard-coded data to game

--- components/Pages/GamePage.tsx
+++ components/Pages/GamePage.tsx
@@ -1,5 +1,5 @@
 import { useAccount } from "wagmi"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { ConnectButton } from "@rainbow-me/rainbowkit"
 import TokenGateModal from "../TokenGateModal"
 import useBalanceOf from "../../hooks/useBalanceOf"
@@ -20,9 +20,39 @@ const GamePage = () => {
     await fetchBalance()
     setMinting(false)
   }
+  // MessageChannel for communication
+  const channel = new MessageChannel()
+
+  function onMessage(event) {
+    // Handle incoming messages here
+    console.log("Received message:", event.data)
+  }
+
+  function callGodotFunction() {
+    const iframe = document.querySelector("#godotGame") as HTMLIFrameElement
+    if (!iframe) {
+      console.error("Iframe not found!")
+      return
+    }
+
+    console.log(iframe)
+    console.log("onload called")
+
+    // Listen for messages on port1
+    channel.port1.onmessage = onMessage
+
+    // Transfer port2 to the iframe
+    // [heart, camera, money]
+    iframe.contentWindow.postMessage([1, 0, 0], "*", [channel.port2])
+
+    console.log("WE DID IT ISAIAH")
+  }
 
   return (
     <div>
+      <button type="button" className="text-white" onClick={callGodotFunction}>
+        SEND DATA TO GODOT
+      </button>
       {openModal && (
         <TokenGateModal title={isConnected ? "unlock" : "connect"}>
           {isConnected ? (
@@ -40,7 +70,13 @@ const GamePage = () => {
           )}
         </TokenGateModal>
       )}
-      <iframe src="/game/index.html" title="Hypersurveilled" className="h-[100vh] w-[100vw]" />
+
+      <iframe
+        id="godotGame"
+        src="/game/index.html"
+        title="Hypersurveilled"
+        className="h-[90vh] w-[100vw]"
+      />
     </div>
   )
 }
