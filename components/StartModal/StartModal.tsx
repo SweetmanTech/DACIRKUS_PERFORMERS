1ff73e4491485473062e52bac9151935eb7b24c3
Sweets Sweetman <sweetmantech@gmail.com>
2023-10-09 11:35:53 -0300

    I add black text to white button.

--- components/StartModal/StartModal.tsx
+++ components/StartModal/StartModal.tsx
@@ -22,7 +22,7 @@ const StartModal = ({ handleClick, children }) => (
         <div className="text-lg md:text-xl text-white uppercase">by Heno</div>
       </div>
       <Button
-        className="text-xl md:text-4xl flex pb-4 md:pb-8 bg-white"
+        className="text-xl md:text-4xl flex pb-4 md:pb-8 bg-white text-black"
         id="play-button"
         onClick={handleClick}
       >
