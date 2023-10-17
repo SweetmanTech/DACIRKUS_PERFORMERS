06824a5565d11bb8da68cb01b279a923840966e3
Sweets Sweetman <sweetmantech@gmail.com>
2023-10-17 20:31:19 -0300

    I split up Spotify Provider into more modular libs.

--- lib/consts.tsx
+++ lib/consts.tsx
@@ -5,3 +5,5 @@ export const SPOTIFY_CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID
 export const SPOTIFY_CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET
 export const SPOTIFY_REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI
 export const SPOTIFY_STATE_KEY = "spotify_auth_state"
+export const RELIEF_TRACK_ID = "5aDNHHNXc16VktqV1gSq23"
+export const HENO_ARTIST_ID = "3mr6jeVpPIXBp8IMMb60aD"
