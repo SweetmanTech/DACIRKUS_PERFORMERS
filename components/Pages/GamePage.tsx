0f962a33de47ee9463aed102bf94048525516ea8
Sweets Sweetman <sweetmantech@gmail.com>
2023-11-18 16:22:26 -0300

    I update game title text.

--- components/Pages/GamePage.tsx
+++ components/Pages/GamePage.tsx
@@ -1,11 +1,8 @@
+import { TITLE } from "../../lib/consts"
+
 const GamePage = () => (
   <div>
-    <iframe
-      id="godotGame"
-      src="/game/index.html"
-      title="Hypersurveilled"
-      className="h-[100vh] w-[100vw]"
-    />
+    <iframe id="godotGame" src="/game/index.html" title={TITLE} className="h-[100vh] w-[100vw]" />
   </div>
 )
 
