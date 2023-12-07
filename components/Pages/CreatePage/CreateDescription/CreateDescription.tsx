818b2aa509b7d9d94b8b9d880c7d7263ca5d9821
Brittney Spalding <brittney.tech.eng@gmail.com>
2023-12-07 14:52:38 -0500

    I complete character modal desktop responsive

--- components/Pages/CreatePage/CreateDescription/CreateDescription.tsx
+++ components/Pages/CreatePage/CreateDescription/CreateDescription.tsx
@@ -2,26 +2,33 @@ import Media from "../../../../shared/Media"
 import WavyText from "../../../WavyText"
 
 const CreateDescription = () => (
-  <div className="px-[30px] py-[20px] text-[20px] font-slimfit text-white">
-    <div className="flex justify-center translate-y-[-20px]">
+  <div
+    className="md:px-[18px] lg:px-[24px] xl:px-[30px] 
+    md:py-[12px] lg:py-[16px] xl:py-[20px] 
+    md:text-[12px] lg:text-[16px] xl:text-[20px] font-slimfit text-white"
+  >
+    <div
+      className="flex justify-center 
+      md:translate-y-[-12px] lg:translate-y-[-16px] xl:translate-y-[-20px]"
+    >
       <Media
         type="image"
         link="/images/Create/logo.png"
         blurLink="/images/Create/logo.png"
-        containerClasses="w-[200px] aspect-[1/1]"
+        containerClasses="md:w-[120px] lg:w-[160px] xl:w-[200px] aspect-[1/1]"
       />
     </div>
-    <div className="translate-y-[-70px] font-bold">
+    <div className="md:translate-y-[-42px] lg:translate-y-[-56px] xl:translate-y-[-70px] font-bold">
       <WavyText
         text="Da Performers"
-        className="text-[#ca4343] text-[54px] drop-shadow-[2px_2px_2px_rgba(0,0,0,1)]"
+        className="text-[#ca4343] xl:text-[54px] lg:text-[43.2px] md:text-[32.4px] drop-shadow-[2px_2px_2px_rgba(0,0,0,1)]"
       />
       <WavyText
         text="Casting call!!!"
-        className="text-[#d0c000] text-[36px] drop-shadow-[2px_2px_2px_rgba(0,0,0,1)] flex justify-center"
+        className="text-[#d0c000] xl:text-[36px] lg:text-[28.8px] md:text-[21.6px] drop-shadow-[2px_2px_2px_rgba(0,0,0,1)] flex justify-center"
       />
     </div>
-    <div className="translate-y-[-60px]">
+    <div className="md:translate-y-[-36px] lg:translate-y-[-48px] xl:translate-y-[-60px]">
       We want you to join Da Cirkus!
       <br />
       -Customize your character
