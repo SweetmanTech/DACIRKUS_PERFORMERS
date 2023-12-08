03eef49883c1df4257b2501d8bcfa1056d84a373
Brittney Spalding <brittney.tech.eng@gmail.com>
2023-12-08 09:16:43 -0500

    I add character model

--- components/Pages/CreatePage/Success/Success.tsx
+++ components/Pages/CreatePage/Success/Success.tsx
@@ -1,10 +1,21 @@
 import WavyText from "../../../WavyText"
+import CharacterModel from "../CharacterModel"
 import SelectedAttributes from "../SelectedAttributes"
 import SuccessButtons from "../SuccessButtons"
 
 const Success = () => (
   <div className="w-full h-full grid grid-cols-2">
-    <div />
+    <div className="relative w-full md:pr-[18px] md:pr-[24px] xl:pr-[30px]">
+      <div className="relative w-full h-full overflow-hidden">
+        <div
+          className="absolute scale-[4.5] h-full w-full left-0 top-0
+        md:translate-y-[612px] lg:translate-y-[816px] xl:translate-y-[1020px] 
+        md:translate-x-[294px] lg:translate-x-[392px] xl:translate-x-[490px]"
+        >
+          <CharacterModel />
+        </div>
+      </div>
+    </div>
     <div
       className="md:py-[18px] lg:py-[24px] xl:py-[30px] 
     md:pl-[42px] lg:pl-[56px] xl:pl-[70px] 
