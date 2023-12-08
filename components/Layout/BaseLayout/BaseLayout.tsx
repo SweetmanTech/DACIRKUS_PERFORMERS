86259951143ae49f55a57364d865ded63a124770
Brittney Spalding <brittney.tech.eng@gmail.com>
2023-12-08 14:39:47 -0500

    I remove black bg

--- components/Layout/BaseLayout/BaseLayout.tsx
+++ components/Layout/BaseLayout/BaseLayout.tsx
@@ -4,7 +4,7 @@ import { ILayout } from "../types"
 const BaseLayout = ({ children }: ILayout) => (
   <div
     className="w-screen h-screen bg-[url('/images/Layout/background.png')] bg-contain
-    flex justify-center"
+    flex justify-center overflow-hidden"
   >
     {children}
   </div>
