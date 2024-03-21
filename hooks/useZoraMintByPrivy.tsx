1df6436ded0c26dd7d5a161e98b640365b4da6d1
SeniorDev <tech.eng.me@gmail.com>
2024-03-21 19:17:02 -0400

    I make mint by privy hook

---
+++ hooks/useZoraMintByPrivy.tsx
@@ -0,0 +1,56 @@
+import handleTxError from "@/lib/handleTxError"
+import useSaleStatus from "./useSaleStatus"
+import { useState } from "react"
+import getZoraFee from "@/lib/viem/getZoraFee"
+import useConnectedWallet from "./useConnectedWallet"
+import usePrivySendTransaction from "./usePrivySendTransaction"
+import { CHAIN_ID, DROP_ADDRESS } from "@/lib/consts"
+import abi from "@/lib/abi/zora-drop.json"
+import { numberToHex } from "viem"
+import { BigNumber } from "ethers"
+
+const useZoraMinByPrivy = () => {
+  const { publicSalePrice } = useSaleStatus()
+  const { connectedWallet } = useConnectedWallet()
+  const { sendTransaction: sendTxByPrivy } = usePrivySendTransaction()
+
+  const [loading, setLoading] = useState(false)
+
+  const mintWithRewards = async () => {
+    try {
+      setLoading(true)
+      const quantity = 1
+      const zoraFee = await getZoraFee(1) as any
+      const comment = "!!!"
+      const mintReferral = process.env.NEXT_PUBLIC_MINT_REFERRAL
+      const args = [connectedWallet, quantity, comment, mintReferral]
+      const price = BigNumber.from(publicSalePrice).add(zoraFee[1]).toString()
+      const hexValue = numberToHex(BigInt(price))
+
+      const response = await sendTxByPrivy(
+        DROP_ADDRESS,
+        CHAIN_ID,
+        abi,
+        "mintWithRewards",
+        args,
+        hexValue,
+        "Collect",
+        "Collect",
+      )
+
+      setLoading(false)
+
+      return response
+    } catch (err) {
+      setLoading(false)
+      // eslint-disable-next-line no-console
+      console.error(err)
+      handleTxError(err)
+      return { error: err }
+    }
+  }
+
+  return { mintWithRewards, loading }
+}
+
+export default useZoraMinByPrivy
