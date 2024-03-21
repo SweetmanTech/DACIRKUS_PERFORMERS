536305bfa7d6ee46364d2c7d334c5179e3195835
SeniorDev <tech.eng.me@gmail.com>
2024-03-21 19:16:35 -0400

    I add chain with chainId

--- lib/consts.tsx
+++ lib/consts.tsx
@@ -1,6 +1,7 @@
 import { base, baseSepolia } from "viem/chains"
 
-export const CHAIN_ID = process.env.NEXT_PUBLIC_TESTNET ? baseSepolia.id : base.id
+export const CHAIN = process.env.NEXT_PUBLIC_TESTNET ? baseSepolia : base
+export const CHAIN_ID = CHAIN.id
 export const SPOTIFY_CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID
 export const SPOTIFY_CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET
 export const SPOTIFY_REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI
@@ -9,3 +10,4 @@ export const RELIEF_TRACK_ID = "5aDNHHNXc16VktqV1gSq23"
 export const HENO_ARTIST_ID = "3mr6jeVpPIXBp8IMMb60aD"
 export const TITLE = "DaPerformers"
 export const ARTIST = "DaCirkus"
+export const DROP_ADDRESS = process.env.NEXT_PUBLIC_DROP_ADDRESS
