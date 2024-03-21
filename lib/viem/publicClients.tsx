558a14dc270faed156b27dd9e62ac65c8305df40
SeniorDev <tech.eng.me@gmail.com>
2024-03-21 19:27:49 -0400

    I fix build error

--- lib/viem/publicClients.tsx
+++ lib/viem/publicClients.tsx
@@ -2,6 +2,6 @@ import { createPublicClient, http } from "viem"
 import { CHAIN } from "../consts"
 
 export const publicClient = createPublicClient({
-  chain: CHAIN,
+  chain: CHAIN as any,
   transport: http(),
 })
