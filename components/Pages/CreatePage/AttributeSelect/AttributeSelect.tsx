bd28792834500a917b08b8826a501e50ba0a57f1
SeniorDev <tech.eng.me@gmail.com>
2024-04-23 09:06:24 -0400

    I add background selector

--- components/Pages/CreatePage/AttributeSelect/AttributeSelect.tsx
+++ components/Pages/CreatePage/AttributeSelect/AttributeSelect.tsx
@@ -1,11 +1,12 @@
 import { useCharacter } from "@/providers/CharacterProvider"
 import { useEffect } from "react"
+import { CBGCOLORS } from "@/lib/character"
 import AttributeButtons from "../AttributeButtons"
 import CharacterModel from "../CharacterModel"
 import MenuButtons from "../MenuButtons"
 
 const AttributeSelect = () => {
-  const { randomAttr } = useCharacter()
+  const { randomAttr, cBG } = useCharacter()
 
   useEffect(() => {
     randomAttr()
@@ -22,6 +23,16 @@ const AttributeSelect = () => {
         <MenuButtons />
       </div>
       <div className="relative w-full md:pr-[18px] md:pr-[24px] xl:pr-[30px]">
+        <div
+          className="absolute 
+          md:left-[18px] lg:left-[24px] xl:left-[30px]
+          md:top-[9px] lg:top-[12px] xl:top-[15px]
+          md:w-[calc(100%-30px)] lg:w-[calc(100%-40px)] xl:w-[calc(100%-50px)] 
+          md:h-[calc(100%-18px)] lg:h-[calc(100%-24px)] xl:h-[calc(100%-30px)]"
+          style={{
+            background: CBGCOLORS[cBG],
+          }}
+        />
         <div className="relative w-full h-full md:ml-[-30px] lg:ml-[-40px] xl:ml-[-50px] ">
           <div className="relative w-full h-full overflow-hidden">
             <div
