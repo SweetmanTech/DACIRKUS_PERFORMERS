ba2b4f2d9370991df41ee208d04d3de5edaa0de3
SeniorDev <tech.eng.me@gmail.com>
2024-04-26 11:16:19 -0400

    I change position

--- pages/api/metadata/index.tsx
+++ pages/api/metadata/index.tsx
@@ -31,11 +31,11 @@ export default async function handler(req: any, res: any) {
   const finalAttribute = metadata?.attributes || deterministicAttribute
   const sheet = metadata?.sheet
 
-  const endpoint = `api/og?tokenId=${tokenId}&&type=${sheet?.type || type}&&skin=${
+  const endpoint = `api/og?type=${sheet?.type || type}&&skin=${
     sheet?.skin || skin
   }&&acc=${sheet?.acc || acc}&&eye=${sheet?.eye || eye}&&hair=${sheet?.hair || hair}&&color=${
     sheet?.color || color
-  }&&outfit=${sheet?.outfit || outfit}&&bg=${sheet?.bg || bg}`
+  }&&outfit=${sheet?.outfit || outfit}&&bg=${sheet?.bg || bg}&&tokenId=${tokenId}`
 
   const metaData = {
     name: `Performer #${tokenId}`,
