72a59a834e3449a1ca447806f702782e157db52f
SeniorDev <tech.eng.me@gmail.com>
2024-05-22 13:00:03 -0400

    I add hook

---
+++ hooks/useIsWhitelist.tsx
@@ -0,0 +1,12 @@
+import { whitelist } from "@/lib/consts"
+import useConnectedWallet from "./useConnectedWallet"
+
+const useIsWhitelist = () => {
+    const { externalWallet } = useConnectedWallet()
+
+    const isWhitelist = whitelist.includes(externalWallet?.address)
+
+    return isWhitelist
+}
+
+export default useIsWhitelist
