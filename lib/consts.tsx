a959a8031ea8962c3b56e693ec31db764e856f2b
SeniorDev <tech.eng.me@gmail.com>
2024-05-22 12:59:42 -0400

    I add while list

--- lib/consts.tsx
+++ lib/consts.tsx
@@ -20,3 +20,13 @@ export const ZORA_COLLECTION_URL = IS_TESTNET
 export const DOMAIN_URL = "https://daperformers.com"
 export const COMMENT = "Da Performers are coming!!!"
 export const MINT_REFERRAL = process.env.NEXT_PUBLIC_MINT_REFERRAL
+
+export const whitelist = [
+  "0x2d9cBC4ECFBd1B8F66Aa798FD51585AE058dAA8B",
+  "0x7A631B4a6b1D0a6b1e433d92690d7C8Aba4F23b4",
+  "0xD6C09962E907428112069273d5C0dc861e7B1C57",
+  "0x5a5811E0A22695FEC271fe908E2a1A64Dc3b06F9",
+  "0x254768D47Cf8958a68242ce5AA1aDB401E1feF2B",
+  "0xb5acDED340D66678f01097818940A0F028DAFB8d", // sweetman.eth
+  "0x99Fc221482ca78664d0288FfC6531cb6dfEB0c5a"  // dacirkus.eth
+]
