915c2a5299728a53a6280ab44b7f8ac3320120a0
TahirHameed74 <tsb_125@yahoo.com>
2024-05-24 23:09:18 +0500

    Terms Page

--- pages/terms/index.tsx
+++ pages/terms/index.tsx
@@ -4,7 +4,7 @@ import React from "react"
 const TermSection = ({ title, content }) => (
   <section className="space-y-4">
     <h2 className="font-semibold text-2xl">{title}</h2>
-    {content.map((paragraph: string, index: number) => (
+    {content.map((paragraph, index) => (
       <p key={index} className="text-lg">
         {paragraph}
       </p>
