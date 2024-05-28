8fe8186216eb2e754d210a2716d519135964d7f3
TahirHameed74 <tsb_125@yahoo.com>
2024-05-28 22:43:43 +0500

    animation - fix slight horizontal movement

--- components/Pages/CreatePage/CharacterModel/Animation.tsx
+++ components/Pages/CreatePage/CharacterModel/Animation.tsx
@@ -4,47 +4,30 @@ import CharacterModel from "./CharacterModel"
 const Animation = () => {
   const { currentFrame } = useCharacterAnimation()
 
+  const baseStyle = `
+    absolute scale-[3.5] h-full w-full left-0 top-0
+    md:translate-x-[100px] lg:translate-x-[134px] xl:translate-x-[168px]`
+
+  const frames = [
+    { id: "frame1", style: "md:translate-y-[500px] lg:translate-y-[670px] xl:translate-y-[840px]" },
+    { id: "frame2", style: "md:translate-y-[510px] lg:translate-y-[680px] xl:translate-y-[850px]" },
+    { id: "frame3", style: "md:translate-y-[520px] lg:translate-y-[690px] xl:translate-y-[860px]" },
+    { id: "frame4", style: "md:translate-y-[530px] lg:translate-y-[700px] xl:translate-y-[870px]" },
+  ]
+
   return (
     <div
-      className="absolute top-0 md:w-[279px] lg:w-[372px] xl:w-[465px] 
+      className="absolute md:w-[279px] lg:w-[372px] xl:w-[465px] z-50
         md:h-[358.8px] lg:h-[478.4px] xl:h-[598px] overflow-hidden
         md:left-[-115.8px] lg:left-[-154.4px] xl:left-[-193px]"
     >
-      {currentFrame === 0 && (
-        <div
-          className="absolute scale-[3.5] h-full w-full left-0 top-0
-            md:translate-y-[510px] lg:translate-y-[680px] xl:translate-y-[850px] 
-            md:translate-x-[258px] lg:translate-x-[344px] xl:translate-x-[430px]"
-        >
-          <CharacterModel isSingle />
-        </div>
-      )}
-      {currentFrame === 1 && (
-        <div
-          className="absolute scale-[3.5] h-full w-full left-0 top-0
-            md:translate-y-[510px] lg:translate-y-[680px] xl:translate-y-[850px] 
-            md:translate-x-[100.8px] lg:translate-x-[134.4px] xl:translate-x-[168px]"
-        >
-          <CharacterModel isSingle />
-        </div>
-      )}
-      {currentFrame === 2 && (
-        <div
-          className="absolute scale-[3.5] h-full w-full left-0 top-0
-            md:translate-y-[510px] lg:translate-y-[680px] xl:translate-y-[850px] 
-            md:translate-x-[-56.4px] lg:translate-x-[-75.2px] xl:translate-x-[-94px]"
-        >
-          <CharacterModel isSingle />
-        </div>
-      )}
-      {currentFrame === 3 && (
-        <div
-          className="absolute scale-[3.5] h-full w-full left-0 top-0
-            md:translate-y-[510px] lg:translate-y-[680px] xl:translate-y-[850px] 
-            md:translate-x-[-214.2px] lg:translate-x-[-285.6px] xl:translate-x-[-357px]"
-        >
-          <CharacterModel isSingle />
-        </div>
+      {frames.map(
+        (frame) =>
+          currentFrame === frames.indexOf(frame) && (
+            <div className={`${baseStyle} ${frame.style}`} key={frame.id}>
+              <CharacterModel isSingle />
+            </div>
+          ),
       )}
     </div>
   )
