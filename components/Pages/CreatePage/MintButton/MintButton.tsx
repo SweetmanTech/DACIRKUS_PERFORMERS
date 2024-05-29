1aa8be1caddb773cbf8f9be26769a36d57c738c2
TahirHameed74 <tsb_125@yahoo.com>
2024-05-29 18:58:12 +0500

    prevented redirect to success page

--- components/Pages/CreatePage/MintButton/MintButton.tsx
+++ components/Pages/CreatePage/MintButton/MintButton.tsx
@@ -7,19 +7,9 @@ const MintButton = () => {
 
   const handleMint = async () => {
     setLoading(true)
-    const result = await singleMint()
-
-    if ((result as { error: any })?.error) {
-      setLoading(false)
-      return { error: true }
-    }
+    await singleMint()
+    setLoading(false)
   }
-
-  // const handleMint = async () => {
-  //   setLoading(true)
-  //   await singleMint()
-  //   setLoading(false)
-  // }
 
   return (
     <button
