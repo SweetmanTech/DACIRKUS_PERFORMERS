b3f5aef6482177546e30c0ee7403b4d396946dcd
TahirHameed74 <tsb_125@yahoo.com>
2024-06-03 00:57:38 +0500

    opensea description urls

--- pages/api/metadata/index.tsx
+++ pages/api/metadata/index.tsx
@@ -1,13 +1,13 @@
 import getDeterministricAttributes from "@/lib/getDeterministricAttributes"
 import getAttributes from "@/lib/getAttributes"
 import { CACCS, CBGNAMES, CCOLORS, CEYES, CHAIRS, COUTFITS, CSKINS, CTYPES } from "@/lib/character"
-import { CHAIN_ID, DOMAIN_URL, DROP_ADDRESS } from "@/lib/consts"
+import { DOMAIN_URL, DROP_ADDRESS } from "@/lib/consts"
 import tokenMinted from "@/lib/tokenMinted"
 import getMetadata from "@/lib/firebase/getMetadata"
 import getIpfsLink from "@/lib/getIpfsLink"
 
 export default async function handler(req: any, res: any) {
-  const { tokenId, chainId } = req.query
+  const { tokenId } = req.query
 
   const isMinted = await tokenMinted(DROP_ADDRESS, tokenId)
   if (!isMinted) return res.status(500).json({ message: "Not minted yet!" })
@@ -28,18 +28,16 @@ export default async function handler(req: any, res: any) {
     CBGNAMES[bg],
   )
 
-  const metadata = response[`${chainId || CHAIN_ID}`]
+  const metadata = response["CHAIN_ID"]
   const finalAttribute = metadata?.attributes || deterministicAttribute
-  const pfp = metadata?.pfp
-  const ss = metadata?.ss
 
-  const pfpUrl = pfp ? getIpfsLink(pfp) : `${DOMAIN_URL}/api/image/${tokenId}`
-  const sheetUrl = ss ? getIpfsLink(ss) : `${DOMAIN_URL}/api/spritesheet/${tokenId}`
+  const imageUrl = `${DOMAIN_URL}/api/image/${tokenId}`
+  const sheetUrl = `${DOMAIN_URL}/api/spritesheet/${tokenId}`
 
   const metaData = {
     name: `Performer #${tokenId}`,
-    image: pfpUrl,
-    description: `PFP Image: ${pfpUrl} \nSpritesheet: ${sheetUrl}`,
+    image: imageUrl,
+    description: `Spritesheet: ${sheetUrl}`,
     attributes: finalAttribute,
   }
 
