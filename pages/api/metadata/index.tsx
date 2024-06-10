3fcad5e5ac116821fd36c308218e370f118e1585
TahirHameed74 <tsb_125@yahoo.com>
2024-06-10 06:46:06 +0500

    metadata for items purchased on thirdweb or etherscan

--- pages/api/metadata/index.tsx
+++ pages/api/metadata/index.tsx
@@ -4,40 +4,94 @@ import { CACCS, CBGNAMES, CCOLORS, CEYES, CHAIRS, COUTFITS, CSKINS, CTYPES } fro
 import { CHAIN_ID, DOMAIN_URL, DROP_ADDRESS } from "@/lib/consts"
 import tokenMinted from "@/lib/tokenMinted"
 import getMetadata from "@/lib/firebase/getMetadata"
+import addMetadata from "@/lib/firebase/addMetadata"
+import getIpfsLink from "@/lib/getIpfsLink"
 
-export default async function handler(req: any, res: any) {
+export default async function handler(req, res) {
   const { tokenId, chainId } = req.query
 
   const isMinted = await tokenMinted(DROP_ADDRESS, tokenId)
   if (!isMinted) return res.status(500).json({ message: "Not minted yet!" })
 
-  const [type, skin, acc, eye, hair, color, outfit, bg] = getDeterministricAttributes(
-    parseInt(tokenId, 10),
-  )
+  let response: { error?: any } & { [key: number]: any } = {}
 
-  const response = (await getMetadata(tokenId)) as any
-  const deterministicAttribute = getAttributes(
-    CTYPES[type],
-    CSKINS[skin],
-    CACCS[acc],
-    CEYES[eye],
-    CHAIRS[hair],
-    CCOLORS[color],
-    COUTFITS[outfit],
-    CBGNAMES[bg],
-  )
+  try {
+    response = (await getMetadata(tokenId)) as any
 
-  const metadata = response[`${chainId || CHAIN_ID}`]
-  const finalAttribute = metadata?.attributes || deterministicAttribute
-  const imageUrl = `${DOMAIN_URL}/api/image/${tokenId}`
-  const sheetUrl = `${DOMAIN_URL}/api/spritesheet/${tokenId}`
+    if (!response) {
+      const [type, skin, acc, eye, hair, color, outfit, bg] = getDeterministricAttributes(
+        parseInt(tokenId, 10),
+      )
+      const deterministicAttribute = getAttributes(
+        CTYPES[type],
+        CSKINS[skin],
+        CACCS[acc],
+        CEYES[eye],
+        CHAIRS[hair],
+        CCOLORS[color],
+        COUTFITS[outfit],
+        CBGNAMES[bg],
+      )
+      response = {
+        [CHAIN_ID]: {
+          attributes: deterministicAttribute,
+          sheet: { type, skin, acc, eye, hair, color, outfit, bg },
+          pfp: "",
+          ss: "",
+        },
+      }
+      await addMetadata(tokenId, deterministicAttribute, {
+        type,
+        skin,
+        acc,
+        eye,
+        hair,
+        color,
+        outfit,
+        bg,
+      })
+    }
 
-  const metaData = {
-    name: `Performer #${tokenId}`,
-    image: imageUrl,
-    description: `Spritesheet: ${sheetUrl}`,
-    attributes: finalAttribute,
+    const metadata = response[`${chainId || CHAIN_ID}`]
+    const [type, skin, acc, eye, hair, color, outfit, bg] = getDeterministricAttributes(
+      parseInt(tokenId, 10),
+    )
+    const deterministicAttribute = getAttributes(
+      CTYPES[type],
+      CSKINS[skin],
+      CACCS[acc],
+      CEYES[eye],
+      CHAIRS[hair],
+      CCOLORS[color],
+      COUTFITS[outfit],
+      CBGNAMES[bg],
+    )
+
+    const finalAttribute = metadata?.attributes || deterministicAttribute
+    const pfp = metadata?.pfp
+    const sheet = metadata?.sheet
+    const ss = metadata?.ss
+
+    const endpoint = `api/og?type=${sheet?.type || type}&skin=${sheet?.skin || skin}&acc=${
+      sheet?.acc || acc
+    }&eye=${sheet?.eye || eye}&hair=${sheet?.hair || hair}&color=${sheet?.color || color}&outfit=${
+      sheet?.outfit || outfit
+    }&bg=${sheet?.bg || bg}&tokenId=${tokenId}`
+
+    const pfpUrl = pfp ? getIpfsLink(pfp) : `${DOMAIN_URL}/${endpoint}`
+    const sheetUrl = ss
+      ? getIpfsLink(ss)
+      : `${DOMAIN_URL}/spritesheet/${chainId || CHAIN_ID}/${tokenId}`
+
+    const metaData = {
+      name: `Performer #${tokenId}`,
+      image: pfpUrl,
+      description: `PFP: ${pfpUrl} \n Spritesheet: ${sheetUrl}`,
+      attributes: finalAttribute,
+    }
+
+    return res.status(200).json(metaData)
+  } catch (error) {
+    return res.status(500).json({ error: "An error occurred while fetching metadata." })
   }
-
-  return res.status(200).json(metaData)
 }
