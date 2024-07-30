6a7e78e86f641c4ac51959dfc088607b481ae08b
SeniorDev <tech.eng.me@gmail.com>
2024-07-30 14:43:37 -0400

    I update spritesheet link

--- pages/api/metadata/index.tsx
+++ pages/api/metadata/index.tsx
@@ -32,7 +32,6 @@ export default async function handler(req: any, res: any) {
   const finalAttribute = metadata?.attributes || deterministicAttribute
   const pfp = metadata?.pfp
   const sheet = metadata?.sheet
-  const ss = metadata?.ss
 
   const endpoint = `api/og?type=${sheet?.type || type}&skin=${sheet?.skin || skin}&acc=${
     sheet?.acc || acc
@@ -41,9 +40,7 @@ export default async function handler(req: any, res: any) {
   }&bg=${sheet?.bg || bg}&tokenId=${tokenId}`
 
   const pfpUrl = pfp ? getIpfsLink(pfp) : `${DOMAIN_URL}/${endpoint}`
-  const sheetUrl = ss
-    ? getIpfsLink(ss)
-    : `${DOMAIN_URL}/spritesheet/${chainId || CHAIN_ID}/${tokenId}`
+  const sheetUrl = `${DOMAIN_URL}/api/og/spritesheet?tokenId=${tokenId}`
 
   const metaData = {
     name: `Performer #${tokenId}`,
