cc07bc1a6358babf7db66c9ff1f706c463c5cd59
SeniorDev <tech.eng.me@gmail.com>
2024-08-02 10:31:55 -0400

    I update nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["cdn.sanity.io", "pbs.twimg.com", "f005.backblazeb2.com"],
+    },
+};
 
 export default nextConfig;
