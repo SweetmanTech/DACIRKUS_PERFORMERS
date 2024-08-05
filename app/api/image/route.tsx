fa1f8f513f350b1abfc8e49103805e376cc2a758
SeniorDev <tech.eng.me@gmail.com>
2024-08-05 13:09:32 -0400

    I fix feedback

--- app/api/image/route.tsx
+++ app/api/image/route.tsx
@@ -1,13 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
-import { ImageResponse } from "@vercel/og"
 import { NextRequest, NextResponse } from "next/server"
 import tokenMinted from "@/lib/tokenMinted"
 import { CHAIN_ID, DROP_ADDRESS } from "@/lib/consts"
 import getMetadata from "@/lib/firebase/getMetadata"
 import getIpfsLink from "@/lib/getIpfsLink"
-
-export const runtime = "edge"
-export const dynamic = "force-dynamic"
 
 export async function GET(req: NextRequest) {
   const queryParams = req.nextUrl.searchParams
@@ -21,19 +17,19 @@ export async function GET(req: NextRequest) {
   const isMinted = await tokenMinted(DROP_ADDRESS, tokenId)
   if (!isMinted) return NextResponse.json({ message: "Not minted yet!" })
 
-  return new ImageResponse(
-    (
-      <img
-        src={pfp}
-        style={{
-          width: 480,
-        }}
-        alt="not found"
-      />
-    ),
-    {
-      width: 480,
-      height: 480,
+  const response = await fetch(pfp)
+
+  if (!response.ok) {
+    throw new Error("failed to fetch image")
+  }
+
+  const imageBuffer = await response.arrayBuffer()
+
+  return new NextResponse(imageBuffer, {
+    status: 200,
+    headers: {
+      "Content-Type": "image/png",
+      "Cache-Control": "s-maxage=86400, stale-while-revalidate",
     },
-  )
+  })
 }
